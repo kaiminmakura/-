4feef0dec8230c377b97e829e33971044eb8ae15
秋山壱樹 <akiayamaitsuki@akiyamaichijunoMacBook-Pro.local>
2020-05-12 11:47:37 +0900

    [finish]2-9

---
+++ fizz_buzz.rb
@@ -0,0 +1,29 @@
+def fizz_buzz(number)
+
+	if number % 3 == 0
+
+		'Fizz'
+
+	elsif number % 5 == 0
+
+		'Buzz'
+
+	elsif number % 15 == 0
+
+		'FizzBuzz'
+
+	else
+
+		number.to_s
+
+	end
+
+end
+
+puts "１以上の数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は、、、"
+
+puts fizz_buzz(input)
